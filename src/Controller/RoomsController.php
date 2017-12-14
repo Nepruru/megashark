8581996db3066be855ec4ed9654162bc62a06630
xavier dankiw <xavier.dankiw@etud.u-picardie.fr>
2017-12-14 08:56:17 +0000

    dernière séance

--- src/Controller/RoomsController.php
+++ src/Controller/RoomsController.php
@@ -6,6 +6,7 @@ use App\Controller\AppController;
 /**
  * Rooms Controller
  *
+ * @property \App\Model\Table\RoomsTable $Rooms
  *
  * @method \App\Model\Entity\Room[] paginate($object = null, array $settings = [])
  */
@@ -14,6 +15,7 @@ class RoomsController extends AppController
 
     /**
      * Index method
+     *
      * @return \Cake\Http\Response|void
      */
     public function index()
@@ -34,13 +36,70 @@ class RoomsController extends AppController
     public function view($id = null)
     {
         $room = $this->Rooms->get($id, [
-            'contain' => []
+            'contain' => ['Showtimes']
         ]);
-        
 
         $this->set('room', $room);
         $this->set('_serialize', ['room']);
         
+        $lundi = strotime('monday this week');
+        $mardi = strotime('+1 this week');
+        $mercredi = strotime('+2 this week');
+        $jeudi = strotime('+3 this week');
+        $vendredi = strotime('+4 this week');
+        $samedi = strotime('+5 this week');
+        $dimanche = strotime('+6 this week');
+        $lundisuiv = strotime('+7 this week');
+        
+        
+        
+        $query = $movie->find('Showtimes')
+            ->where(['room_id'=>$id])
+            ->where(['debut <='=>$lundi])
+            ->where(['debut >='=>$mardi]);
+        $query = $movie->find('Showtimes')
+            ->where(['room_id'=>$id])
+            ->where(['debut >='=>$mardi])
+            ->where(['debut <'=>$mercredi]);
+        $query = $movie->find('Showtimes')
+            ->where(['room_id'=>$id])
+            ->where(['debut <='=>$mercredi])
+            ->where(['debut >='=>$jeudi]);
+        $query = $movie->find('Showtimes')
+            ->where(['room_id'=>$id])
+            ->where(['debut <='=>$jeudi])
+            ->where(['debut >='=>$vendredi]);
+        $query = $movie->find('Showtimes')
+            ->where(['room_id'=>$id])
+            ->where(['debut <='=>$vendredi])
+            ->where(['debut >='=>$samedi]);
+        $query = $movie->find('Showtimes')
+            ->where(['room_id'=>$id])
+            ->where(['debut <='=>$samedi])
+            ->where(['debut >='=>$dimanche]);
+    $query = $movie->find('Showtimes')
+            ->where(['room_id'=>$id])
+            ->where(['debut <='=>$dimanche])
+            ->where(['debut >='=>$lundisuiv]);
+            
+    $this->set('semaine',[0=>$lundiag,
+                          1=>$mardiag,
+                          2=>$mercrediag,
+                          3=>$jeudiag,
+                          4=>$vendrediag,
+                          5=>$samediag,
+                          7=>$dimancheag,
+                          8=>$ludisuivag]);
+                          
+    $this->set('jours',[0=>'lundi'.date('d-m-Y',$lundi),
+                        1=>'mardi'.date('d-m-Y',$mardi),
+                        2=>'mercredi'.date('d-m-Y',$mercredi),
+                        3=>'jeudi'.date('d-m-Y',$jeudi),
+                        4=>'vendredi'.date('d-m-Y',$vendredi),
+                        5=>'samedi'.date('d-m-Y',$samedi),
+                        6=>'dimanche'.date('d-m-Y',$dimanche),
+                        5=>'lundisuiv'.date('d-m-Y',$lundisuiv)]);
+                        
     }
 
     /**
